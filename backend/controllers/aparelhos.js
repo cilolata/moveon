8ed36318185f62f578e504a50407ecbd2e7abef4
cilolata <cilolata@gmail.com>
2020-04-03 21:27:08 -0300

    crud aparelhos

--- backend/controllers/aparelhos.js
+++ backend/controllers/aparelhos.js
@@ -2,6 +2,16 @@ const modelAparelhos = require('../models/modelAparelhos');
 
 
 module.exports = app => {
+
+    app.get('/aparelhos', (req, res) => {
+        modelAparelhos.lista(res);
+    });
+
+    app.get('/aparelhos/:id', (req, res) => {
+        const id = parseInt(req.params.id);
+
+        modelAparelhos.buscaPorId(id, res);
+    });
    
     app.post('/aparelhos', (req, res) => {
         const aparelho = req.body;
@@ -9,5 +19,18 @@ module.exports = app => {
         modelAparelhos.adiciona(aparelho, res);
     });
 
+    app.put('/aparelhos/:id', (req, res)=>{
+        const id = parseInt(req.params.id);
+        const valores = req.body;
+
+        modelAparelhos.altera(id, valores, res);
+    });
+
+    app.delete('/aparelhos/:id', (req, res)=>{
+        const id = parseInt(req.params.id);
+
+        modelAparelhos.deleta(id, res);
+    });
+
     
-}
+};
--- backend/models/modelAparelhos.js
+++ backend/models/modelAparelhos.js
@@ -13,7 +13,57 @@ class modelAparelhos {
                 res.status(201).json(aparelho);
             }
         })
-    }
+    };
+
+    lista(res){
+        const sql = 'SELECT * FROM aparelhos';
+
+        conexao.query(sql, (erro, resultados)=>{
+            if(erro){
+                res.status(400).json(erro)
+            }else{
+                res.status(200).json(resultados);
+            }
+        })
+    };
+
+    buscaPorId(id, res){
+        const sql = `SELECT * FROM aparelhos WHERE id=${id}`;
+
+        conexao.query(sql, (erro, resultados)=>{
+            const aparelho = resultados[0];
+
+            if(erro){
+                res.status(400).json(erro)
+            }else{
+                res.status(200).json(aparelho);
+            }
+        })
+    };
+  
+    altera(id, valores, res){
+        const sql = 'UPDATE aparelhos SET ? WHERE id=?';
+
+        conexao.query(sql, [valores, id], (erro, resultados)=>{
+            if(erro){
+                res.status(400).json(erro)
+            }else{
+                res.status(200).json({...valores, id});
+            }
+        })
+    };
+
+    deleta(id, res){
+        const sql = 'DELETE FROM aparelhos WHERE id=?';
+
+        conexao.query(sql, id, (erro, resultados) => {
+            if(erro){
+                res.status(400).json(erro)
+            }else{
+                res.status(200).json({id});
+            }
+        })
+    };
 
 
 };
