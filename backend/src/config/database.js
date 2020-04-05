f823652b7ff7be33f6ddad9a355e8ca83ec18380
cilolata <cilolata@gmail.com>
2020-04-05 18:00:03 -0300

    mudança na sintaxe para ecma6

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,12 +1,10 @@
-module.exports = {
-  dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'admin',
-  database: 'moveon',
-  define: {
-    timestamps: true,
-    underscored: true,
-    underscoredAll: true,
-  },
+export const dialect = 'postgres';
+export const host = 'localhost';
+export const username = 'postgres';
+export const password = 'admin';
+export const database = 'moveon';
+export const define = {
+  timestamps: true,
+  underscored: true,
+  underscoredAll: true,
 };
