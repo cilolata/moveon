dd6a46a5b0495fb9c71c8857df22dccc4d6cc16f
irailtonreis <irailton.oliveira91@gmail.com>
2020-04-06 14:33:42 -0300

    feat:empresas: add new methods controller

--- backend/src/app/controllers/EmpresaController.js
+++ backend/src/app/controllers/EmpresaController.js
@@ -1,6 +1,23 @@
 import Empresa from '../models/Empresa';
 
 class EmpresaController {
+  async index(req, res) {
+    const empresas = await Empresa.findAll();
+    return res.json(empresas);
+  }
+
+  async show(req, res) {
+    const { id } = req.params;
+
+    const empresa = await Empresa.findOne({
+      where: {
+        id,
+      },
+    });
+
+    return res.json(empresa);
+  }
+
   async store(req, res) {
     const empresaExists = await Empresa.findOne({
       where: { email: req.body.email },
@@ -13,6 +30,26 @@ class EmpresaController {
     const empresa = await Empresa.create(req.body);
 
     return res.json(empresa);
+  }
+
+  async update(req, res) {
+    const { id } = req.params;
+
+    const empresa = await Empresa.findByPk(id);
+
+    await empresa.update(req.body);
+
+    return res.json(empresa);
+  }
+
+  async delete(req, res) {
+    const { id } = req.params;
+
+    const empresa = await Empresa.findByPk(id);
+
+    await empresa.destroy();
+
+    return res.send();
   }
 }
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,5 +5,9 @@ import EmpresaController from './app/controllers/EmpresaController';
 const routes = new Router();
 
 routes.post('/empresas', EmpresaController.store);
+routes.get('/empresas', EmpresaController.index);
+routes.get('/empresas/:id', EmpresaController.show);
+routes.put('/empresas/:id', EmpresaController.update);
+routes.delete('/empresas/:id', EmpresaController.delete);
 
 export default routes;
