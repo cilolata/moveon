9aaa206e521de98a2513b2579250f4c8143b88d6
cilolata <cilolata@gmail.com>
2020-04-21 22:07:57 -0300

    criando o password bcrypt de user

--- backend/src/app/models/User.js
+++ backend/src/app/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcrypt';
 
 class User extends Model {
   static init(sequelize) {
@@ -9,6 +10,7 @@ class User extends Model {
         data_nascimento: Sequelize.DATE,
         cpf: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
       },
       {
@@ -16,7 +18,17 @@ class User extends Model {
       }
     );
 
+    this.addHook('beforeSave', async user => {
+      if(user.password){
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
     return this;
+  }
+
+  checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
   }
 
   static associate(models) {
